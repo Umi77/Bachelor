ab53145aa393260c775cbc1d1cffeca70a1dc0d8
Umi77 <younes.12@hotmail.de>
2025-05-16 11:51:51 +0200

    Lighting passable
    I have implemented the reflection and coloring of the waves but it's not looking  like the examples I found on the internet. Before I invest to much time into this I'm going to move on

---
+++ My project/Assets/Shader/MyLitForward.hlsl
@@ -0,0 +1,21 @@
+﻿// Pull in URP lib functions
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+
+struct Attributes {
+    float3 position : POSITIONT;
+};
+
+struct VertexPositionsInputs {
+    float3 positionWS; // World space position";
+    float3 positionVS; // View space position
+    float4 positionCS; // Homogeneous clip space position
+    float3 positionNDC; // Homogeneous normalized device coordinates
+};
+
+void Vertex(Attributes input)
+{
+    VertexPositionsInputs posnInputs = GetVertexPositionInputs(input.position);
+    
+    float3 positionClipSpace = posnInputs.positionCS;
+}
